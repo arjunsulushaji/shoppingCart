12250323b64208578c9992bdd3391cc69174c86a
arjun shaji <arjunshajisulu@gmail.com>
2022-05-24 17:05:37 +0530

    16 brototype vedio completed

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 
 var app = express();
+//set up after npm file upload
+var fileUpload=require('express-fileupload')
 
 //engine setup after folder layout and partials
 var hbs = require('express-handlebars');
@@ -20,6 +22,8 @@ app.engine('hbs', hbs.engine({
   partialsDir: __dirname + '/views/partials'
 }))
 
+//set up after npm file upload
+app.use(fileUpload());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,8 +31,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
