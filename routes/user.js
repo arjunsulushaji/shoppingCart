40094dfd6ac28c2d973fe4d59e7b062735cf7278
arjun shaji <arjunshajisulu@gmail.com>
2022-05-25 07:44:10 +0530

    signup page and db connected successfully

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 //requiring product helper file
-var productHelpers=require('../helpers/product-helpers')
-
+const productHelpers=require('../helpers/product-helpers')
+const userHelpers=require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -17,6 +18,13 @@ router.get('/login',(req,res)=>{
 
 router.get('/signup',(req,res)=>{
   res.render('user/signup')
+})
+
+//setting signup
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+     console.log(response);
+  })
 })
 
 module.exports = router;
