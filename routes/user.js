a9088127823263d5e532598a45d4b7da2f4c75cc
arjun shaji <arjunshajisulu@gmail.com>
2022-06-11 07:27:35 +0530

    cart total completed successfully

--- routes/user.js
+++ routes/user.js
@@ -72,8 +72,9 @@ router.get('/logout', (req, res) => {
 router.get('/cart', async (req, res) => {
   if (req.session.loggedIn) {
     let products = await userHelpers.getCartProducts(req.session.user._id)
-    console.log(products)
-    res.render('user/cart', { products, user: req.session.user })
+    let cartTotal = await userHelpers.getTotalAmount(req.session.user._id)
+    //console.log(cartTotal)
+    res.render('user/cart', { products, user: req.session.user,cartTotal})
   } else {
     res.redirect('/login')
   }
